2a67455a2d7abc1e3cdb4df6bea3a593a4e8e421
adriaurora <adrianlab@proton.me>
2025-06-24 22:14:28 +0200

    added sys.argv

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from stats import count_words
-from stats import count_characters
+from stats import (count_words, count_characters, sort_characters)
+import sys
 
 def get_book_text(book_path):
     with open(book_path) as file:
@@ -7,10 +7,22 @@ def get_book_text(book_path):
     return book_text
 
 def main():
-    book_path = 'books/frankenstein.txt'
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     book_text = get_book_text(book_path)
     book_characters = count_characters(book_text)
-    print(f'{count_words(book_text)} words found in the document.')
-    print(book_characters)
+    sorted_characters = sort_characters(book_characters)
+    printing(sorted_characters, book_path, book_text)
+
+def printing(sorted_characters, book_path, book_text):
+    print("============ BOOKBOT ============")
+    print(f'Analyzing book found at {book_path}...')
+    print("----------- Word Count ----------")
+    print(f'Found {count_words(book_text)} total words')
+    print("---------- Character Count -------")
+    for character in sorted_characters:
+        print(f"{character["char"]}: {character["num"]}")
 
 main()
--- stats.py
+++ stats.py
@@ -1,6 +1,7 @@
 def count_words(book_text):
     words = book_text.split()
     return len(words)
+
 def count_characters(book_text):
     characters_dict = {}
     for words in book_text:
@@ -12,3 +13,22 @@ def count_characters(book_text):
                 else:
                     characters_dict[character] = 1
     return characters_dict
+
+def get_order(characters_dict):
+    return characters_dict["num"]
+
+def sort_characters(characters_dict):
+    characters_list_dictionary = []
+    for character in characters_dict:
+        characters_list_dictionary.append({"char": character, "num": characters_dict[character]})
+    characters_list_dictionary.sort(reverse=True, key=get_order)
+    return characters_list_dictionary
+
+
+def sort_characters2(characters_dict):
+    characters_list_dictionary = []
+    for character, value in characters_dict.items():
+        character_dictionary= {"char": character, "num": value}
+        characters_list_dictionary.append(character_dictionary)
+    characters_list_dictionary.sort(reverse=True, key=get_order)
+    return characters_list_dictionary
